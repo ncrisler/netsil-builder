a2c04b58b2f9a042d8273f085c39a97f2143319c
luhkevin <luhkevin@gmail.com>
2016-12-07 01:09:15 -0800

    GCE and Azure builders (#1)
    * wip
    
    * add azure
    
    * shell works with azure coreos
    
    * got past shell and ansible copy
    
    * parameterize interface and ip address
    
    * parameterize ssh user
    
    * only azure and fix template bug
    
    * change resources
    
    * build type logic
    
    * remove client secret
    
    * remove unnecessary setting
    
    * typos

--- Dockerfile
+++ Dockerfile
@@ -10,9 +10,9 @@ RUN apt-get -qq update && \
     ansible ca-certificates curl qemu qemu-utils ssh-client unzip vim
 
 # Download and extract the Packer binary.
-RUN curl -sOL https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip && \
-    unzip packer_0.10.1_linux_amd64.zip && \
-    rm -f packer_0.10.1_linux_amd64.zip
+RUN curl -sOL https://releases.hashicorp.com/packer/0.11.0/packer_0.11.0_linux_amd64.zip && \
+    unzip packer_0.11.0_linux_amd64.zip && \
+    rm -f packer_0.11.0_linux_amd64.zip
 
 # Copy builder files into the image.
 COPY . /opt/builder/
--- start.sh
+++ start.sh
@@ -15,6 +15,7 @@ fi
 export PACKER_LOG_PATH="/opt/packer.log"
 export PACKER_LOG="${PACKER_LOG:-0}"
 export ANSIBLE_SCP_IF_SSH=1
+export AZURE_CLIENT_SECRET="${AZURE_CLIENT_SECRET:-""}"
 export IMAGE_PATH="${IMAGE_PATH:-/opt/images}"
 export IMAGE_NAME="${IMAGE_NAME:-base-dcos}"
 export AWS_REGION="${AWS_REGION:-us-west-2}"
@@ -24,8 +25,11 @@ if [ "$BUILD_TYPE" == "amazon-ebs" ]; then
     BUILD_TYPE="amazon-ebs"
 elif [ "$BUILD_TYPE" == "qemu" ]; then
     BUILD_TYPE="qemu"
+elif [ "$BUILD_TYPE" == "azure-arm" ] ; then
+    BUILD_TYPE="azure-arm"
+    export ANSIBLE_SUDO_FLAGS="-H -S"
 elif [[ -n "$AWS_ACCESS_KEY" && -n "$AWS_SECRET_KEY" ]]; then
-    BUILD_TYPE="qemu,amazon-ebs"
+    BUILD_TYPE="qemu,amazon-ebs,azure-arm"
 fi
 
 # Local environment variables
@@ -57,7 +61,7 @@ echo "$SSH_PUBLIC_KEY_TEXT" | write_user_data > \
      "data/openstack/latest/user_data"
 
 # Run the Packer build.
-/opt/builder/packer build -only=$BUILD_TYPE packer-config.json
+PACKER_LOG=1 /opt/builder/packer build -var ssh_pass="Password1234" -only=$BUILD_TYPE packer-config.json
 
 # The image ouput directory must not exist on subsequent runs, so the directory
 # is renamed with a datestamp.
