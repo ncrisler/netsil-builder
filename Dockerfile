075d910946b12da6da02985b484ab5eed86d1de0
luhkevin <luhkevin@gmail.com>
2016-12-11 02:46:58 -0800

    fixes (#2)

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,13 @@ RUN apt-get -qq update && \
     apt-get -qq install -y --no-install-recommends \
     ansible ca-certificates curl qemu qemu-utils ssh-client unzip vim
 
-# Download and extract the Packer binary.
+# Download and extract the Packer binary (0.10.2).
+RUN mkdir -p /opt/builder-0-10-2 && cd /opt/builder-0-10-2 && \
+    curl -sOL https://releases.hashicorp.com/packer/0.10.2/packer_0.10.2_linux_amd64.zip && \
+    unzip packer_0.10.2_linux_amd64.zip && \
+    rm -f packer_0.10.2_linux_amd64.zip && \
+    cd /opt/builder
+
 RUN curl -sOL https://releases.hashicorp.com/packer/0.11.0/packer_0.11.0_linux_amd64.zip && \
     unzip packer_0.11.0_linux_amd64.zip && \
     rm -f packer_0.11.0_linux_amd64.zip
--- start.sh
+++ start.sh
@@ -15,21 +15,20 @@ fi
 export PACKER_LOG_PATH="/opt/packer.log"
 export PACKER_LOG="${PACKER_LOG:-0}"
 export ANSIBLE_SCP_IF_SSH=1
-export AZURE_CLIENT_SECRET="${AZURE_CLIENT_SECRET:-""}"
+export ANSIBLE_SUDO_FLAGS="-H -S"
+export AZURE_CLIENT_SECRET=$(cat /opt/secrets/azure-client-secret)
 export IMAGE_PATH="${IMAGE_PATH:-/opt/images}"
 export IMAGE_NAME="${IMAGE_NAME:-base-dcos}"
 export AWS_REGION="${AWS_REGION:-us-west-2}"
 
-# Include AWS AMI build if AWS credentials are specified.
-if [ "$BUILD_TYPE" == "amazon-ebs" ]; then
-    BUILD_TYPE="amazon-ebs"
-elif [ "$BUILD_TYPE" == "qemu" ]; then
-    BUILD_TYPE="qemu"
-elif [ "$BUILD_TYPE" == "azure-arm" ] ; then
-    BUILD_TYPE="azure-arm"
-    export ANSIBLE_SUDO_FLAGS="-H -S"
-elif [[ -n "$AWS_ACCESS_KEY" && -n "$AWS_SECRET_KEY" ]]; then
-    BUILD_TYPE="qemu,amazon-ebs,azure-arm"
+packer_binary=/opt/builder/packer
+
+if [ "${BUILD_TYPE}" == "googlecompute" ] ; then
+    packer_binary=/opt/builder-0-10-2/packer
+fi
+
+if [ "${BUILD_TYPE}" == "all" ] ; then
+    BUILD_TYPE="qemu,amazon-ebs,googlecompute,azure-arm"
 fi
 
 # Local environment variables
@@ -61,7 +60,7 @@ echo "$SSH_PUBLIC_KEY_TEXT" | write_user_data > \
      "data/openstack/latest/user_data"
 
 # Run the Packer build.
-PACKER_LOG=1 /opt/builder/packer build -var ssh_pass="Password1234" -only=$BUILD_TYPE packer-config.json
+PACKER_LOG=1 ${packer_binary} build -var ssh_pass="Password1234" -only=$BUILD_TYPE packer-config.json
 
 # The image ouput directory must not exist on subsequent runs, so the directory
 # is renamed with a datestamp.
