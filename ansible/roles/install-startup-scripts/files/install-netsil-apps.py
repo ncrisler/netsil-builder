12795fcadafcd51bf22d1e6d3f6d74a5519e1021
luhkevin <luhkevin@gmail.com>
2017-09-06 15:28:02 -0700

    Refactor for cloud (#38)
    * seed node stuff
    
    * separate master slave
    
    * change roles
    
    * asdf
    
    * move version-info
    
    * change user
    
    * need to add some vars
    
    * ansible python interpreter
    
    * Add restarts
    
    * scale
    
    * only install and scale cloud apps
    
    * fixes for startup and scale
    
    * fixes install netsil apps
    
    * jq
    
    * configure redhat
    
    * inject credentials
    
    * add to cloud deploy role
    
    * apps dir
    
    * install realtime in cloud
    
    * add cloud worker join script
    
    * rename
    
    * setup worker role
    
    * dont really need setup worker...
    
    * add mesos resources for ha
    
    * replace build_type with is_ha var
    
    * ha stuff

--- ansible/roles/install-startup-scripts/files/install-netsil-apps.py
+++ ansible/roles/install-startup-scripts/files/install-netsil-apps.py
@@ -2,10 +2,17 @@ import httplib
 import json
 import time
 import os
+import argparse
 
 MARATHON_HOST = os.environ.get('MARATHON_HOST', failobj='marathon.mesos')
 MARATHON_PORT = os.environ.get('MARATHON_PORT', failobj='8080')
+MESOS_HOST = os.environ.get('MESOS_HOST', failobj='leader.mesos')
+MESOS_PORT = os.environ.get('MESOS_PORT', failobj='5050')
 APPS_DIR = os.environ.get('APPS_DIR', failobj='/opt/netsil/latest/apps/build/specs')
+IS_HA = os.environ.get('IS_HA', failobj='no')
+
+exclude_in_ha = ['ceph-osd', 'ceph-rgw', 'ceph-monitor']
+exclude_in_singlenode = ['druid-middle-manager', 'tranquility']
 
 def wait(count, step, timeout):
     time.sleep(step)
@@ -37,6 +44,63 @@ def wait_for_marathon():
             print "Could not connect to marathon..."
             count = wait(count, step, timeout)
 
+def get_total_workers():
+    timeout=1800
+    count=0
+    step=10
+    while True:
+        try:
+            conn = httplib.HTTPConnection(MESOS_HOST + ':' + MESOS_PORT)
+            conn.request('GET', '/metrics/snapshot')
+            resp = conn.getresponse()
+            respBody = resp.read()
+            respObj = json.loads(respBody)
+            total_workers = int(respObj['master/slaves_active'])
+            print "Total workers is: " + str(total_workers)
+            return total_workers
+        except:
+            print "Could not connect to marathon..."
+            count = wait(count, step, timeout)
+
+def do_scale(app_json):
+    if 'env' in app_json:
+        app_env = app_json['env']
+        return 'DO_SCALE' in app_env and app_env['DO_SCALE'] == 'yes'
+    else:
+        return False
+
+def filter_apps(all_apps, filter_list):
+    for app in filter_list:
+        app_file = app + '.json'
+        if app_file in all_apps:
+            all_apps.remove(app_file)
+    return all_apps
+
+def list_apps():
+    all_apps = os.listdir(APPS_DIR)
+    if IS_HA == 'yes':
+        return filter_apps(all_apps, exclude_in_ha)
+    else:
+        return filter_apps(all_apps, exclude_in_singlenode)
+
+def scale_netsil_apps():
+    total_workers = get_total_workers()
+    scale_json = {'instances': total_workers}
+    for app in list_apps():
+        with open(APPS_DIR + '/' + app, 'rb') as app_file:
+            app_json = json.load(app_file)
+            if 'id' in app_json and do_scale(app_json):
+                app_id = app_json['id']
+                conn = httplib.HTTPConnection(MARATHON_HOST + ':' + MARATHON_PORT)
+                conn.request('PUT', '/v2/apps/' + str(app_id), json.dumps(scale_json), {"Content-type": "application/json"})
+                resp = conn.getresponse()
+                status = resp.status
+                if status == 200:
+            	    print "Scaled app!"
+                else:
+                    print "Issue scaling app: " + str(app_id)
+                    exit(1)
+
 # Solely to get past the wait-time
 def restart_app(app_id):
     timeout=1800
@@ -59,7 +123,7 @@ def restart_app(app_id):
             count = wait(count, step, timeout)
 
 def install_netsil_apps():
-    for app in os.listdir(APPS_DIR):
+    for app in list_apps():
         with open(APPS_DIR + '/' + app, 'rb') as app_file:
             app_json = json.load(app_file)
             if 'id' in app_json:
@@ -81,10 +145,39 @@ def install_netsil_apps():
                 print "Error: " + str(resp.read())
                 exit(1)
 
-def main():
-    wait_for_marathon()
-    install_netsil_apps()
+def wait_for_deployments():
+    deploy_period=10
+    deploy_count=0
+    deploy_max=900
 
+    # Initial sleep for deployments to queue up
+    time.sleep(deploy_period)
+    while deploy_count < deploy_max:
+        try:
+            conn = httplib.HTTPConnection(MARATHON_HOST + ':' + MARATHON_PORT)
+            conn.request('GET', '/v2/deployments')
+            resp = conn.getresponse()
+            respBody = resp.read()
+            if respBody != '[]':
+                print "There are still pending deployments"
+                time.sleep(deploy_period)
+            else:
+                break
+        except:
+            print "Could not connect to marathon..."
+            exit(1)
+
+def main(action):
+    wait_for_marathon()
+    if action == 'install':
+        install_netsil_apps()
+    elif action == 'scale':
+        wait_for_deployments()
+        scale_netsil_apps()
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='install action')
+    parser.add_argument('-a', '--action', default='install', dest='action', type=str, nargs='?')
+    args = parser.parse_args()
+    main(args.action)
+
--- template.sh
+++ template.sh
@@ -7,5 +7,6 @@ APPS_DIR=${APPS_DIR:-""}
 docker build -t ${URI_NAMESPACE}/netsil-builder ${DIR}
 
 docker run -e "INVENTORY=[agents]\nlocalhost ansible_connection=local" \
+       -e "IS_HA=${IS_HA}" \
        -v ${APPS_DIR}:/apps \
        -t ${URI_NAMESPACE}/netsil-builder /opt/builder/start-template.sh
