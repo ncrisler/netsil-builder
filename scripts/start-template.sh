7b2637d4f7b5febf30acaddb896a83a34f4b6122
Kevin Lu <kevin@netsil.com>
2017-10-30 14:32:56 -0700

    change hosts directory

--- scripts/start-template.sh
+++ scripts/start-template.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+ansible_config_dir=/opt/ansible/etc
+# Create ansible config directory
+mkdir -p ${ansible_config_dir}
 
 # Set inventory
-echo -e ${INVENTORY} > /home/user/hosts
+echo -e ${INVENTORY} > ${ansible_config_dir}/hosts
 
 # Run ansible
-ansible-playbook -i /home/user/hosts /opt/builder/ansible/template.yml
+ansible-playbook -i ${ansible_config_dir}/hosts /opt/builder/ansible/template.yml
