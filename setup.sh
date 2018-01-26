56d353fed33b579dc682cf07f71c7e6c33eba8d3
Kevin Lu <luhkevin@gmail.com>
2018-01-26 11:12:08 -0800

    version checking

--- setup.sh
+++ setup.sh
@@ -51,10 +51,6 @@ function abs_path() {
     )
 }
 
-function detect_linux_distrib() {
- 
-}
-
 function check_docker() {
     (command -v docker || docker) > /dev/null 2>&1
     if [ "$?" -ne 0 ]; then
@@ -100,9 +96,63 @@ function check_jq() {
     fi
 }
 
+function check_ubuntu() {
+    
+}
+
+function check_coreos() {
+    
+}
+
+function check_centos() {
+    
+}
+
 function check_by_distrib() {
     # Do checks based on linux distribution
+    if [ -f /etc/os-release ]; then
+        # freedesktop.org and systemd
+        . /etc/os-release
+        OS=$ID
+        VER=$VERSION_ID
+    elif type lsb_release >/dev/null 2>&1; then
+        # linuxbase.org
+        OS=$(lsb_release -si)
+        VER=$(lsb_release -sr)
+    elif [ -f /etc/lsb-release ]; then
+        # For some versions of Debian/Ubuntu without lsb_release command
+        . /etc/lsb-release
+        OS=$DISTRIB_ID
+        VER=$DISTRIB_RELEASE
+    elif [ -f /etc/debian_version ]; then
+        # Older Debian/Ubuntu/etc.
+        OS=Debian
+        VER=$(cat /etc/debian_version)
+    elif [ -f /etc/SuSe-release ]; then
+        # Older SuSE/etc.
+        ...
+    elif [ -f /etc/redhat-release ]; then
+        # Older Red Hat, CentOS, etc.
+        ...
+    else
+        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+        OS=$(uname -s)
+        VER=$(uname -r)
+    fi
 
+    echo "OS is $OS and Version is $VER"
+    if [ "$OS" = "Ubuntu" ] ; then
+        if [ "$VER" = "16.04" ] ; then
+            check_ubuntu
+        else
+            echo "Exiting. This script only supports Ubuntu 16.04"
+            exit 1
+        fi
+    elif [ "$OS" = "coreos" ] ; then
+        check_coreos
+    elif [ "$OS" = "rhel" ] || [ "$OS" = "centos" ] ; then
+        check_centos
+    fi
 }
 
 ###########################################################
@@ -246,11 +296,6 @@ if [ ! -d "${APPS_DIR}" ]; then
     display_usage
     exit 1
 fi
-
-#################################
-### Detect linux distribution ###
-#################################
-detect_linux_distrib
 
 ###################################
 ### Perform prerequisite checks ###
