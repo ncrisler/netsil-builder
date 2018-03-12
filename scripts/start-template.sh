ac6ddcbb3c927e4e7a063dcb29655edf83c60858
Kevin Lu <kevin@netsil.com>
2018-03-12 16:16:28 -0700

    fix build by fixing permissions

--- scripts/start-template.sh
+++ scripts/start-template.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ansible_config_dir=/opt/ansible/etc
 # Create ansible config directory
 mkdir -p ${ansible_config_dir}
@@ -8,3 +9,6 @@ echo -e ${INVENTORY} > ${ansible_config_dir}/hosts
 
 # Run ansible
 ansible-playbook -i ${ansible_config_dir}/hosts /opt/builder/ansible/template.yml
+
+# chown so permissions are correct (this fixes build)
+chown -R ${LOCAL_USER_ID}:${LOCAL_USER_ID} /apps
